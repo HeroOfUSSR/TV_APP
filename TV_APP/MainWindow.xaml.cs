a3f99bf2e8502dbf230a5f070b472c66c39a4de4
Роман Задумин <rrooma256@mail.ru>
2024-02-20 22:27:28 +0300

    Видео тест

--- TV_APP/MainWindow.xaml.cs
+++ TV_APP/MainWindow.xaml.cs
@@ -1,13 +1,5 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
+using TV_APP.WPFFORMS;
 
 namespace TV_APP
 {
@@ -19,6 +11,12 @@ namespace TV_APP
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SecondWindow  secondWindow = new SecondWindow();
+            secondWindow.Show();    
         }
     }
 }
---
+++ TV_APP/WPFFORMS/SecondWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+
+
+namespace TV_APP.WPFFORMS
+{
+    /// <summary>
+    /// Логика взаимодействия для SecondWindow.xaml
+    /// </summary>
+    public partial class SecondWindow : Window
+    {
+        public SecondWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
