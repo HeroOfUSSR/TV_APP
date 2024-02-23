0ab77f15b10201d758084ed262e36caaad054c47
ALEXE <ALEXE@DESKTOP-DDO84UQ>
2024-02-23 22:58:07 +0300

    Подключил погодную апишку

---
+++ TV_APP/OpenWeather/main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_APP.OpenWeather
+{
+    class main
+    {
+        public double temp;
+
+        public double feels_like;
+
+        public double temp_min;
+
+        public double temp_max;
+
+        public int pressure;
+        
+        public int humidity;
+    }
+}
---
+++ TV_APP/OpenWeather/weather.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_APP.OpenWeather
+{
+    public class weather
+    {
+        public int id;
+
+        public string main;
+
+        public string description;
+
+        public string icon;
+
+        public Bitmap Icon
+        {
+            get
+            {
+                WebRequest request = WebRequest.Create($"https://openweathermap.org/img/wn/{icon}@2x.png");
+
+                WebResponse response = request.GetResponse();
+
+                Stream responseStream = response.GetResponseStream();
+
+                Bitmap bitmapImg = new Bitmap(responseStream);
+
+                return bitmapImg;
+
+            }
+        }
+    }
+}
