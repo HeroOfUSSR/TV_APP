b1d814d59b8834b96dc943161acaddf1cc099136
ALEXE <ALEXE@DESKTOP-DDO84UQ>
2024-02-25 16:36:36 +0300

    Готовое

---
+++ TV_APP/OpenWeather/wind.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TV_APP.OpenWeather
+{
+    class wind
+    {
+        public int speed;
+
+        public int deg;
+    }
+}
