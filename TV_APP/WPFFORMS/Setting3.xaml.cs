f322fc0b3516b752214e2535132ac20c43f713d8
Student_ORIT <Student_ORIT@ORIT-14>
2024-02-29 12:03:15 +0300

    bruh2

--- TV_APP/WPFFORMS/Setting3.xaml.cs
+++ TV_APP/WPFFORMS/Setting3.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -13,6 +15,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Drawing;
 
 namespace TV_APP.WPFFORMS
 {
@@ -21,6 +24,7 @@ namespace TV_APP.WPFFORMS
     /// </summary>
     public partial class Setting3 : Window
     {
+        DBConnection db = new DBConnection();
 
         static readonly ImageSourceConverter imageSourceConverter
             = new ImageSourceConverter();
@@ -30,7 +34,7 @@ namespace TV_APP.WPFFORMS
             InitializeComponent();
         }
 
-        private void imageButton_Click(object sender, RoutedEventArgs e)
+        public void imageButton_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
             openFileDialog.Filter = "Media files (*.BMP; *.JPEG; *.PNG; *.TIFF)|*.BMP; *.JPEG; *.PNG; *.TIFF";
@@ -40,6 +44,59 @@ namespace TV_APP.WPFFORMS
             }
 
             imagePreview.Source = (ImageSource)imageSourceConverter.ConvertFrom(new Uri(openFileDialog.FileName));
+
+           BitmapImage bitmapImage = openFileDialog;
+        }
+
+        private void imageButton2_Click(object sender, RoutedEventArgs e)
+        {
+            var eventName = nameEventTextbox.Text;
+            var eventDate = dateEventPicker.SelectedDate;
+
+
+            var eventImage = BufferFromImage(imagePreview.Source);
+            db.OpenConnection();
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            var currentDate = DateTime.Now;
+            DataTable table = new DataTable();
+
+            string request = $"INSERT INTO Events (Name_Event, Date_Event, Picture_Event) VALUES () Events WHERE Date_Event={currentDate.ToString("yyyyMMdd")}";
+
+            SqlCommand comm = new SqlCommand(request, db.GetConnection());
+
+            adapter.SelectCommand = comm;
+
+            adapter.Fill(table);
+
+            SqlDataReader reader = comm.ExecuteReader();
+
+            while (reader.Read())
+            {
+                if (table.Rows.Count > 0)
+                {
+                    newsLabel.Content = reader["Name_Event"].ToString();
+                }
+            }
+
+            db.CloseConnection();
+        }
+
+        public byte[] BufferFromImage(BitmapImage imageSource)
+        {
+            Stream stream = imageSource.StreamSource;
+            byte[] buffer = null;
+
+            if (stream != null && stream.Length > 0)
+            {
+                using (BinaryReader br = new BinaryReader(stream))
+                {
+                    buffer = br.ReadBytes((Int32)stream.Length);
+                }
+            }
+
+            return buffer;
         }
     }
 }
